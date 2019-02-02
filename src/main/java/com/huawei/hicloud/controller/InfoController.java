60a50d253906c9e8bdf503ddfca7906963c8ccab
Sunflower <970440700@qq.com>
2019-02-02 17:41:18 +0800

    config

--- src/main/java/com/huawei/hicloud/controller/InfoController.java
+++ src/main/java/com/huawei/hicloud/controller/InfoController.java
@@ -3,6 +3,7 @@ package com.huawei.hicloud.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/v1")
+@RefreshScope
 public class InfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(InfoController.class);
@@ -22,12 +24,16 @@ public class InfoController {
     @Value("${server.port}")
     private Integer port;
 
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public Map<String, Object> getInfo() {
         logger.info("### Get info ...");
         HashMap<String, Object> result = new HashMap<>();
         result.put("port", port);
         result.put("name", name);
+        result.put("useLocalCache", useLocalCache);
 
         return result;
     }
